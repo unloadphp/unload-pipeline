412f254368e90405892ae763cc4ffd62a703683b
yhortuk <17699384+yhortuk@users.noreply.github.com>
2023-01-22 15:29:42 +0200

    fix: failed unload binary download and installation

--- GitHub-Actions/one-stage-pipeline-template/{{cookiecutter.outputDir}}/.github/install-unload.sh
+++ GitHub-Actions/one-stage-pipeline-template/{{cookiecutter.outputDir}}/.github/install-unload.sh
@@ -3,12 +3,15 @@
 UNLOAD_VERSION=$1
 
 # install composer
-export PATH=~/.composer/vendor/bin:$PATH
+export TMP_UNLOAD_BIN="/tmp/unload"
+mkdir $TMP_UNLOAD_BIN
+export PATH="~/.composer/vendor/bin:$TMP_UNLOAD_BIN:$PATH"
 
 # check for packaged binary
 if [ -f "unload" ]; then
     echo "Packaged binary exists...skip package installation"
-    mv ./unload /usr/bin/unload
+    chmod +x unload
+    mv ./unload /tmp/unload/unload
     unload --version
     return
 fi
@@ -16,6 +19,5 @@ fi
 # install unload
 curl -L https://github.com/unloadphp/unload/releases/download/$UNLOAD_VERSION/unload -o unload
 chmod +x unload
-export CURRENT_DIR=$(pwd)
-export PATH="$CURRENT_DIR:$PATH"
+mv ./unload /tmp/unload/unload
 unload --version
